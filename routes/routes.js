12e193825d6240846f0104898ddb26a2c2f2b91d
maddy <bradfieldm@mail.com>
2018-06-01 11:54:00 -0600

    yes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var fs = require('fs');
-var router = express.Router();
-var fileData = JSON.parse(fs.readFileSync("data/merch.json", "utf8"));
+var fileData = JSON.parse(fs.readFileSync("./data/data.json", "utf8"));
 
-var router = require('./routes/routes');
-app.use("/", router);
+var mongoClient = mongodb.MongoClient;
+var url = "mongodb://localhost:27017";
+var databaseName = "userData"
+
+var router = express.Router();
 
 router.route("/").get(
     function (req, res) {
